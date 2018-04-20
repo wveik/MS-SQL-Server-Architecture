fc968949d338b4f177c43f88809dff8c8b982255
Mihail <kataev.06@gmail.com>
2018-04-20 14:10:47 +0300

    Исправил крокозябры в тексте

--- SQL Скрипты-Helper/XML - обход items.sql
+++ SQL Скрипты-Helper/XML - обход items.sql
@@ -1,11 +1,11 @@
 DECLARE	@Doc XML =
 '
 <Order>
-	<Customer Name="Åâãåíèé Îíåãèí" />
+	<Customer Name="Евгений Онегин" />
 	<Products>
-		<Item Name="Òåëåôîí Nokia" Price="100" Quantity="3" />
-		<Item Name="Òåëåôîí iPhone" Price="200" Quantity="2" />
-		<Item Name="Òåëåôîí Motorola" Price="100" Quantity="1" />
+		<Item Name="Телефон Nokia" Price="100" Quantity="3" />
+		<Item Name="Телефон iPhone" Price="200" Quantity="2" />
+		<Item Name="Телефон Motorola" Price="100" Quantity="1" />
 	</Products>
 </Order>
 '
@@ -15,6 +15,8 @@ SELECT	@Doc.query ('/Order/Products/Item[@Price="100"][2]')
 SELECT T.c.query('.') AS result  
 FROM   @Doc.nodes('/Order/Products/Item') T(c)  
 GO  
+
+
 
 DECLARE	@Doc1 XML =
 '
