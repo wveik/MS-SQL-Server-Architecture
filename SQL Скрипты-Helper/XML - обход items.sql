e5ee6eb5c7c815691ee3990f1dc3ed2ff1c50fda
Mihail <kataev.06@gmail.com>
2018-04-20 13:24:45 +0300

    XML - обход items

---
+++ SQL Скрипты-Helper/XML - обход items.sql
@@ -0,0 +1,18 @@
+DECLARE	@Doc XML =
+'
+<Order>
+	<Customer Name="Евгений Онегин" />
+	<Products>
+		<Item Name="Телефон Nokia" Price="100" Quantity="3" />
+		<Item Name="Телефон iPhone" Price="200" Quantity="2" />
+		<Item Name="Телефон Motorola" Price="100" Quantity="1" />
+	</Products>
+</Order>
+'
+
+SELECT	@Doc.query ('/Order/Products/Item[@Price="100"][2]')
+
+SELECT T.c.query('.') AS result  
+FROM   @Doc.nodes('/Order/Products/Item') T(c)  
+GO  
+
