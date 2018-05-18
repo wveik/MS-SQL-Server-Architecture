1b0b4afe6bcda43ec153b7e2926401ebabecdcc9
Mihail <kataev.06@gmail.com>
2018-05-18 08:30:57 +0300

    REVERSE - ИНВЕРСИЯ ЧЕРЕЗ ПОТОМКА РОДИТЕЛЕЙ

--- SQL Скрипты-Helper/Получения Дерева всех потомков.sql
+++ SQL Скрипты-Helper/Получения Дерева всех потомков.sql
@@ -22,3 +22,25 @@ SELECT * FROM MyTable
 
 --SELECT * FROM MyTable
 --WHERE LEVEL=2
+
+--REVERSE - ИНВЕРСИЯ ЧЕРЕЗ ПОТОМКА РОДИТЕЛЕЙ	
+with MyTable as (
+	SELECT [treenodeid]
+      ,[treecode_r]
+      ,[treenodeparentid]
+      ,[treenodecode]
+      ,[treenodeprocedure]
+      ,[treenoderightcode]
+      ,[treenodedeteted]
+	  , 1 as LEVEL
+  FROM [UNIVERS].[cfg].[treenode]
+  WHERE treenodeparentid =2
+
+	UNION ALL
+
+	SELECT Child.*, Parent.LEVEL + 1
+	 FROM MyTable Parent INNER JOIN [UNIVERS].[cfg].[treenode] Child
+		ON Parent.treenodeparentid=Child.treenodeid
+)
+
+SELECT * FROM MyTable
