380c5868677f9aec87f14be565816828fd71a8c2
Mihail <kataev.06@gmail.com>
2018-05-31 09:39:50 +0300

    DROP TABLE

--- SQL Скрипты-Helper/Ограничение внешнего ключа при удалении таблицы DROP TABLE foreign key constraint.sql
+++ SQL Скрипты-Helper/Ограничение внешнего ключа при удалении таблицы DROP TABLE foreign key constraint.sql
@@ -2,6 +2,9 @@ SELECT *
 FROM sys.foreign_keys
 WHERE referenced_object_id = object_id('[cfg].[tree]')
 
+select * from sys.objects
+where object_id=754817751 --parent_object_id
+
 --and if there are any, with this statement here, you could create SQL statements to actually drop those FK relations:
 
 SELECT 
